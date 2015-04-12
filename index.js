43dde7e46ca1c54c01fff2254b94b381938a99e5
Felipe Sabino <felipe@sabino.me>
2015-04-12 10:10:04 -0700

    parsing message to string before sending to kafka

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var kafka = require('kafka-node'),
     _ = require('lodash'),
 
     Message = require('./lib//message')(_),
-    Publish = require('./lib/publish')(kafka);
+    Publish = require('./lib/publish')(_, kafka);
 
 var middleware = require('./lib/middleware')(kafka, Message, Publish, _);
 
--- tests/publish-tests.js
+++ tests/publish-tests.js
@@ -2,7 +2,8 @@ var chai    = require('chai'),
     expect  = chai.expect,
     assert  = chai.assert,
     kafka = require('kafka-node'),
-    Publish = require('../lib/publish')(kafka);
+    _ = require('lodash'),
+    Publish = require('../lib/publish')(_, kafka);
 
 describe('Publish', function() {
 
@@ -38,6 +39,30 @@ describe('Publish', function() {
     publisher('test', null);
   });
 
+  it('should use parse object message to string before sending', function(done) {
+
+    producer.send = function(payloads, cb) {
+      assert.lengthOf(payloads, 1, 'payloads`s value has a length of 1');
+      assert.isString(payloads[0].messages);
+      done();
+    };
+    var publisher = Publish.generate(producer, {topic: 'my-topic', partition: 2, attributes: 1});
+
+    publisher({"message key": "message value"}, null);
+  });
+
+  it('should not parse object message to string before sending if options says so', function(done) {
+
+    producer.send = function(payloads, cb) {
+      assert.lengthOf(payloads, 1, 'payloads`s value has a length of 1');
+      assert.isNotString(payloads[0].messages);
+      done();
+    };
+    var publisher = Publish.generate(producer, {topic: 'my-topic', partition: 2, attributes: 1, parse_to_json: false});
+
+    publisher({"message key": "message value"}, null);
+  });
+
   it('should create kafka.KeyedMessage if key is provided', function(done) {
 
     producer.send = function(payloads, cb) {
@@ -46,8 +71,35 @@ describe('Publish', function() {
       assert.equal(payloads[0].messages.value, 'test', 'message value is \'test\'');
       done();
     };
-    var publisher = Publish.generate(producer, {topic: 'my-topic', partition: 2, attributes: 1});
+    var publisher = Publish.generate(producer, {topic: 'my-topic', partition: 2, attributes: 1, parse_to_json:false});
 
     publisher('test', 'key');
   });
+
+  it('should use parse object key to string before sending', function(done) {
+
+    producer.send = function(payloads, cb) {
+      assert.lengthOf(payloads, 1, 'payloads`s value has a length of 1');
+      assert.isString(payloads[0].messages.key);
+      done();
+    };
+    var publisher = Publish.generate(producer, {topic: 'my-topic', partition: 2, attributes: 1});
+
+    publisher({"message key": "message value"}, {'key key': 'key value'});
+  });
+
+  it('should not parse object key to string before sending if settings says so', function(done) {
+
+    producer.send = function(payloads, cb) {
+      assert.lengthOf(payloads, 1, 'payloads`s value has a length of 1');
+      assert.isNotString(payloads[0].messages.key);
+      done();
+    };
+    var publisher = Publish.generate(producer, {topic: 'my-topic', partition: 2, attributes: 1, parse_to_json:false});
+
+    publisher({"message key": "message value"}, {'key key': 'key value'});
+  });
+
+
+
 });
